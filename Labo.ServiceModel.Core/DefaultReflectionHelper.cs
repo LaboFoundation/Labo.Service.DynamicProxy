9a014915120f76d0575304a73efe9156fe55d7f3
bakgun <boraa@d-teknoloji.com.tr>
2016-06-29 13:50:05 +0300

    added reflection helper class

---
+++ Labo.ServiceModel.Core/DefaultReflectionHelper.cs
@@ -0,0 +1,74 @@
+﻿namespace Labo.ServiceModel.Core
+{
+    using System;
+    using System.Reflection;
+
+    using Labo.Common.Reflection;
+
+    public sealed class DefaultReflectionHelper : IReflectionHelper
+    {
+        public void SetFieldValue(object instance, string fieldName, object value)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            Type objectType = instance.GetType();
+            FieldInfo fieldInfo = objectType.GetField(fieldName);
+            fieldInfo.SetValue(instance, value);
+        }
+
+        public object GetFieldValue(object instance, string fieldName)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            Type objectType = instance.GetType();
+            FieldInfo fieldInfo = objectType.GetField(fieldName);
+            return fieldInfo.GetValue(instance);
+        }
+
+        public void SetPropertyValue(object instance, string propertyName, object value)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            ReflectionHelper.SetPropertyValue(instance, propertyName, value);
+        }
+
+        public void SetPropertyValue(object instance, PropertyInfo propertyInfo, object value)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            ReflectionHelper.SetPropertyValue(instance, propertyInfo, value);
+        }
+
+        public object GetPropertyValue(object instance, string propertyName)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException("instance");
+            }
+
+            return ReflectionHelper.GetPropertyValue(instance, propertyName);
+        }
+
+        public object CreateInstance(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            return ReflectionHelper.CreateInstance(type);
+        }
+    }
+}
---
+++ Labo.ServiceModel.Core/IReflectionHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Labo.ServiceModel.Core
+{
+    using System;
+    using System.Reflection;
+
+    public interface IReflectionHelper
+    {
+        void SetFieldValue(object instance, string fieldName, object value);
+
+        object GetFieldValue(object instance, string fieldName);
+
+        void SetPropertyValue(object instance, string propertyName, object value);
+
+        void SetPropertyValue(object instance, PropertyInfo propertyInfo, object value);
+
+        object GetPropertyValue(object instance, string propertyName);
+
+        object CreateInstance(Type type);
+    }
+}
