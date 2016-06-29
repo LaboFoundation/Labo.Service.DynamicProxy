4e76d8c8e8559bf7070e204d82a492da1c70a8b7
bakgun <boraa@d-teknoloji.com.tr>
2016-06-29 13:49:35 +0300

    deleted old code

--- Labo.ServiceModel.Core/Utils/Reflection/PropertyAccessItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Labo.ServiceModel.Core.Utils.Reflection
-{
-    internal sealed class PropertyAccessItem
-    {
-        public GetHandler Getter { get; set; }
-
-        public SetHandler Setter { get; set; }
-    }
-}
